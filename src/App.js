4659c6b301f8342f62b2cb14dbe990e97584812d
Jack Hubert <jphubert93@gmail.com>
2021-08-12 14:03:26 -0600

    successfully fetched toDos from the rails backend & set into state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
 import './App.css'
 
+const baseURL = 'http://localhost:3000/to_dos'
+const parseHTTPResponse = response => response.json()
+
 class App extends Component {
   
   state = {
-    todos: []
+    to_dos: []
+  }
+
+  // Lifecycle Method which runs at the beginning of a components lifecycle and
+  // only runs one time
+  componentDidMount(){
+    this.getToDos()
+  }
+  
+  getToDos = () => {
+    fetch( baseURL )
+      .then( parseHTTPResponse )
+      .then( to_dos => this.setState( to_dos ) )
   }
 
 
