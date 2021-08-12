dcd4ee86f292ccdc869320cba85100ffeb9a6db8
Jack Hubert <jphubert93@gmail.com>
2021-08-12 17:58:41 -0600

    added toggler function & useState hook for edit button

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function ToDoItem({ id, title, content, deleteToDo }){
 
+    const [ isToggled, setIsToggled] = useState(false)
     const handleClick = event => deleteToDo(id)
+    const handleToggle = () => setIsToggled( !isToggled )
 
     return (
         <li className="to-do-item">
             <h2>{title}</h2>
             <h3>{content}</h3>
             <button className="delete-button" onClick={handleClick}>DELETE</button>
-            <button className="edit-button" onClick={handleClick}>EDIT</button>
+            <button className="edit-button" onClick={handleToggle}>EDIT</button>
         </li>
     )
 }
