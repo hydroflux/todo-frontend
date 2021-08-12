5b43ea8717b558348ee98472b145605cdaf931f6
Jack Hubert <jphubert93@gmail.com>
2021-08-12 17:10:56 -0600

    added state values to toDoForm; added onChange attribute to inputs & created onChange function to handle changes

--- src/components/ToDoForm.js
+++ src/components/ToDoForm.js
@@ -11,22 +11,29 @@ export default class ToDoForm extends Component {
 
     state = initialState
 
+    handleChange = event => {
+        let { name, value, checked } = event.target
+
+        value = ( name === "urgent" ) ? checked : value
+        this.setState({ [name]: value })
+    }
+
     render() {
+        const { title, content, urgent } = this.state
+
         return (
             <form className="to-do-form">
                 <h2>Create A New To Do Item</h2>
                 <label>Title</label>
-                <input type="text" name="title"/>
+                <input type="text" name="title" value={title} onChange={this.handleChange}/>
                 <label>Content</label>
-                <input type="text" name="text"/>
+                <input type="text" name="text" value={content} onChange={this.handleChange}/>
                 <div className="urgent-input">
                     <label>Urgent</label>
-                    <input type="checkbox" name="urgent"/>
+                    <input type="checkbox" name="urgent" checked={urgent} onChange={this.handleChange}/>
                 </div>
                 <input type="submit" />
             </form>
         )
     }
 }
-
-// Using a class component because controlled forms get their inputs from state
