bb98b3846467e512cf9762bd0b728a708435826b
Jack Hubert <jphubert93@gmail.com>
2021-08-19 12:27:46 -0600

    refactored 'home' component syntax

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,8 +6,14 @@ import ToDoContainer from '../containers/ToDoContainer'
 export default function Home(props) {
     return (
         <>
-            <ToDoForm submitAction={props.addToDo}/>
-            <ToDoContainer to_dos={props.to_dos} submitAction={props.updateToDo} removeToDo={props.removeToDo}/>
+            <ToDoForm
+                submitAction={props.addToDo}
+            />
+            <ToDoContainer 
+                to_dos={props.to_dos}
+                submitAction={props.updateToDo}
+                removeToDo={props.removeToDo}
+            />
         </>
     )
 }
