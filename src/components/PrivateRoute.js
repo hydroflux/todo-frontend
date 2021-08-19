ab5e512949fd09fba112c59aa16e6b31b7d7d317
Jack Hubert <jphubert93@gmail.com>
2021-08-19 12:19:49 -0600

    commmented out console.log from PrivateRoute (want to have it there for future debugging when using PrivateRoute component) & dropped from Home

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Redirect, Route } from 'react-router-dom'
 
 export default function PrivateRoute({ path, component: Component, ...props }) {
-    console.log(props)
+    // console.log(props)
     return localStorage.token
         ? <Route exact path={path} render={ (routerProps) => <Component {...props}/> }/>
         : <Redirect to="/signup" />
